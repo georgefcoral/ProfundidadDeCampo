d7eb9b5ea46dd940f41a20c81a3357a0cfcff122
Jorge Coral Coral <georgefcoral@gmail.com>
2022-03-17 20:43:28 -0600

    Se paso a una funcion la comunicacion serial y el movimiento del motor

--- main/OpenCV-1394/main.cpp
+++ main/OpenCV-1394/main.cpp
@@ -15,35 +15,22 @@ using namespace cv;
 numero positivo la camara se aleja del objeto.
 numero negativo la camara se acerca al objeto.
 */
-const string stepping = "150";
 
+int step = 100;
 /*# de imagenes a*/
 int iter = 30;
 
-
+double getMeasure(int step,serialPOSIX comm1);
 int main(int argc, char **argv)
 {
     Mat frame;
     ofstream MyFile("data.txt");
     serialPOSIX comm1(115200);
     Camera1394 C0(0);
-    ssize_t n;
-    ssize_t n2;
+
     namedWindow("Capturador");
-    string str = "0100";
-    int finish = 0;
-    char *buff = new char[str.length()];  
-    char *data = new char[1]; 
     
-    					/*CAMERA CONFIG*/
-    C0.Print_Cameras_List();
-	C0.Print_Camera_Info();
-	C0.Setup (10, DC1394_VIDEO_MODE_640x480_MONO8, DC1394_ISO_SPEED_800, DC1394_FRAMERATE_60, 10, MONO8);
-	C0.Set_Filename_Base("ChuchitaBolseada");
-	C0.Set_MaxFileCounter(10);
-	C0.Set_FileSaveType(FST_NONE);
-	C0.Set_CaptureInterval (0.5);
-	C0.Get_Capture_Interval();
+    
 	C0.Start_Capture();
     int k = 0;//Its a counter
     string received = "";
@@ -67,41 +54,12 @@ int main(int argc, char **argv)
 	/*Envio de datos a Motor y cambio de cuadros*/
     do
     { 
-
-
         uint32_t idx;
 		uFrame *uF;
         frame = Mat::zeros(Size(640, 480), CV_8UC1);
-        //str = encode(steps[k]);
-        strcpy(buff, stepping.c_str());
-        
-        /*Empieza a enviar Datos*/
-        do{  
-        n = comm1.send(buff);
-        cout<<"Cadena "<<buff<<" enviada"<<endl;
 
-        if (n >= 0)
-            break;
-        
-        }while(1);
-        usleep(1000);
-        /*Termina enviar datos*/
-
-        /*Recibe datos*/
-        received = "";
-        do{
-            data = comm1.receive(1, &n2);
-            if(data!= NULL && n2>=0){                    
-                received += data[0];
-                if((int)data[0] == 0)
-                    finish = 1;
-            }
-            if(finish == 1){
-                finish = 0;
-                break;
-            }
-        }while(1);
-        /*TERMINA RECIBIR DATOS*/
+        double measure = getMeasure(step,comm1);
+        cout<<"Measure: "<<measure<<endl;
 
         /*Empieza Captura*/
 		idx = C0.Get_Last_Captured_Frame_Index();
@@ -116,14 +74,13 @@ int main(int argc, char **argv)
 		     << endl;
 		cout << "uFrame["<< uF->Index << "].timestamp= " << uF->timestamp << endl;
 #endif
-      C0.Release_uFrame_Pointer(idx);
+        C0.Release_uFrame_Pointer(idx);
 		imshow( "Capturador", frame);
         //imwrite("./outputs/"+to_string(k)+".jpg",frame);
 		int ex = waitKeyEx(100);
    	    if (ex>= 0 )
 	   	    break;
-        MyFile <<k<<","<<received<<endl;
-        cout<<"Iteracion "<< k <<" con distancia : "<<received  <<" mm."<<endl;
+
         k++;
         usleep(100);
     } while(k<iter);
@@ -133,10 +90,53 @@ int main(int argc, char **argv)
     // Close the file
     
     MyFile.close();
-    delete [] buff;
+    
     C0.Finish_Capture();
 	C0.FlushStorageBuffer();
     return 0;
 }
 
 
+double getMeasure(int step,serialPOSIX comm1){
+    int finish = 0;
+    ssize_t n;
+    ssize_t n2;
+    string str = "0100";
+    char *data = new char[1]; 
+    string stepping = to_string(step);
+    string received = "";
+    char *buff = new char[str.length()]; 
+
+    strcpy(buff, stepping.c_str());
+        
+            /*Envio de datos*/
+    do{  
+        n = comm1.send(buff);
+        cout<<"Cadena "<<buff<<" enviada"<<endl;
+
+        if (n >= 0)
+            break;
+    
+    }while(1);
+        usleep(1000);
+    /*Termina enviar datos*/
+    /*Recibe datos*/
+    received = "";
+    do{
+        data = comm1.receive(1, &n2);
+        if(data!= NULL && n2>=0){                    
+            received += data[0];
+            if((int)data[0] == 0)
+                finish = 1;
+        }
+        if(finish == 1){
+            finish = 0;
+            break;
+        }
+    }while(1);
+    /*TERMINA RECIBIR DATOS*/
+
+    double measure = stod(received);
+    delete [] buff;
+    return measure;
+}
